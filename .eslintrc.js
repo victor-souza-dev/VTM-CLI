a454dc1b4780fbc93385676398cafc1489b575ea
KlowdyTMS <klowdyacrux@gmail.com>
2023-08-28 09:34:47 -0300

    add prettier and eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,16 @@
  */
 module.exports = {
   parser: '@typescript-eslint/parser',
-
+  trailingComma: 'es6',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
   extends: [
     'plugin:@typescript-eslint/recommended',
-
     'prettier',
     'plugin:prettier/recommended',
   ],
