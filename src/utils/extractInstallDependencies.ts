353089f6619201879ea1fa5580955003b334b57b
victor-souza-dev <victor.souza2210@gmail.com>
2023-09-01 13:18:27 -0300

    throw in extract dependencies

--- src/utils/extractInstallDependencies.ts
+++ src/utils/extractInstallDependencies.ts
@@ -2,6 +2,9 @@ import { IConfigAdapter } from '../adapters/configAdapter'
 import { dependencyInstall } from '../controls/dependencyInstall'
 
 function tratamentValues(value: string): string {
+  if (!value) {
+    return ''
+  }
   return value.toLowerCase().replace(/\s/g, '')
 }
 
@@ -10,19 +13,32 @@ export function extractInstallDependencies(config: IConfigAdapter) {
 
   switch (tratamentValues(config.styled.cssFramework)) {
     case `materialui`:
-      dependencies +=
+      dependencies += `${
         // @ts-ignore
-        dependencyInstall.materialui[tratamentValues(config.styled.cssStyled)] +
-        ``
+        dependencyInstall.materialui[tratamentValues(config.styled.cssStyled)]
+      } `
       break
 
     default:
       Object.keys(config.styled).forEach((value) => {
-        dependencies +=
-          // @ts-ignore
-          dependencyInstall[tratamentValues(config.styled[value])] + ' '
+        // @ts-ignore
+        if (!dependencyInstall[tratamentValues(config.styled[value])]) {
+          throw new Error(
+            `The ${value} contains an invalid value, see valid options in the documentation: https://github.com/TechMinds-Group/VTM-CLI`
+          )
+        }
+
+        if (tratamentValues(config.styled[value]) !== 'none') {
+          dependencies +=
+            // @ts-ignore
+            `${dependencyInstall[tratamentValues(config.styled[value])]} `
+        }
       })
       break
+  }
+
+  if (dependencies === 'npm i ') {
+    return ''
   }
 
   return dependencies.trim()
